44677d46cded46bc23b8d3663e2d2ee7534599f1
Cainan <cainan6697@gmail.com>
2021-07-16 12:14:43 -0300

    Objects

---
+++ Basics/objects.js
@@ -0,0 +1,14 @@
+const product1 = {};
+product1.name = 'MacBook Air';
+product1.price = 1099.00;
+product1['units'] = 6;
+
+console.log(product1);
+
+const product2 = {
+  name: 'Pixel 6 XL',
+  price: 999.00,
+  units: 3,
+};
+
+console.log(product2);
