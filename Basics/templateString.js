d993efab17e65e7e1fac0016824cd8b87d976f89
Cainan <cainan6697@gmail.com>
2021-07-16 11:12:52 -0300

    Tamplate Strings

---
+++ Basics/templateString.js
@@ -0,0 +1,12 @@
+const name = 'Callisto';
+const concatenation = 'Ola ' + name + '!';
+const template = `
+  Ola
+  ${name}!`;
+
+console.log(concatenation, template);
+
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = texto => texto.toUpperCase();
+console.log(`Ei... ${up('cuidado')}!`);
