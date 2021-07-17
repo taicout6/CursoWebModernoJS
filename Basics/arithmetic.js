6118f6ca6da4a5c371ebf63bfd207697a451ae4d
Cainan <cainan6697@gmail.com>
2021-07-16 21:22:09 -0300

    Arithmetic

---
+++ Basics/arithmetic.js
@@ -0,0 +1,8 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+const sum = a + b + c + d;
+const subtract = b - c;
+const multiply = b * d;
+const divide = d / b;
+const modulo = d % b;
+
+console.log(sum, subtract, divide, modulo);
