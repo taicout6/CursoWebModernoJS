ddc84a0f27998def69c0598eb8553b981eae6f18
taicout6 <taicout6@gmail.com>
2022-02-16 18:47:02 -0300

    ifElse

---
+++ EstruturasDeControle/ifElse.js
@@ -0,0 +1,12 @@
+const imprimirResultado = function(nota) {
+  if (nota >= 7) {
+    console.log('Aprovado');
+  } else {
+    console.log('Reprovado');
+  }
+};
+
+imprimirResultado(10);
+imprimirResultado(7);
+imprimirResultado(6);
+imprimirResultado(4);
