afc9515bfc61150a7c9764f0b7ced3fa811803d8
taicout6 <taicout6@gmail.com>
2022-02-25 07:42:41 -0300

    laço de repetição com for

---
+++ EstruturasDeControle/for.js
@@ -0,0 +1,13 @@
+for(let index = 1; index <= 10; index += 1) {
+  console.log(`index = ${index}`);
+}
+
+let contador = 1;
+for(contador; contador <= 5; contador += 1) {
+  console.log(`contador = ${contador}`);
+}
+
+const notas = [6.7, 7.4, 9.9, 8.2, 7.7];
+for(index = 0; index < notas.length; index += 1) {
+  console.log(`nota -> ${notas[index]}`);
+}
